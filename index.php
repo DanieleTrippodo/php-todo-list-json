60b5d74581c2f5f5dda8e759c597ba4b785cc94b
DanieleTrippodo <trippododaniele@gmail.com>
2024-07-01 18:11:04 +0200

    Scaffolding di Base

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+
+    <!-- Collegamneto CSS -->
+    <link rel="stylesheet" href="./style.css">
+
+    <!-- Collegamento CDN Vue-3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- Collegamento CDN Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+
+
+
+
+
+
+<body>
+
+<!-- Contenuto HTML -->
+
+</body>
+
+</html>
